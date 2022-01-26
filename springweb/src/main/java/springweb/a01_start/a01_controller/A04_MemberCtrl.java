6a9e26271654f84747fc73ec660452be464b5e20
JisuLee <lji2509@naver.com>
2022-01-26 18:45:56 +0900

    1/26

--- springweb/src/main/java/springweb/a01_start/a01_controller/A04_MemberCtrl.java
+++ springweb/src/main/java/springweb/a01_start/a01_controller/A04_MemberCtrl.java
@@ -2,30 +2,36 @@ package springweb.a01_start.a01_controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+//springweb.a01_start.a01_controller.A04_MemberCtrl
+//컨테이너에 등록
 // springweb.a01_start.a01_controller.A04_MemberCtrl
-// 컨테이너에 등록합니다.
 
 @Controller
 public class A04_MemberCtrl {
-	// 요청 url 호출
-	// 1. 초기 화면 호출
-	@RequestMapping("/loginFrm.do")
+	//요청 url 호출
+	//1. 초기 화면 호출
+	// http://localhost:8000/springweb/loginFrm.do
+	@GetMapping("/loginFrm.do")
 	public String loginFrm() {
 		return "WEB-INF\\views\\a06_login.jsp";
 	}
-	// 2. 요청값 처리화면 호출. login.do?id=@@@&pass=@@@
-	@RequestMapping("/login.do")
-	public String login(@RequestParam("id") String id, 
-						@RequestParam("pass") String pass, Model d
-			) {
+	
+	
+	
+	
+	//2. 요청값 처리화면 호출 login.do?id=@@@&pass=@@@
+	@PostMapping("/login.do")
+	public String login(@RequestParam("id") String id, @RequestParam("pass") String pass, Model d) {
 		System.out.println("#로그인#");
-		System.out.println("id:"+id);
-		System.out.println("pass:"+pass);
-		String result = (id.equals("himan")&&pass.equals("7777"))?"로그인성공":"로그인 실패";
-		System.out.println("결과:"+result);
+		System.out.println("id : " + id);
+		System.out.println("pass : " + pass);
+		String result = (id.equals("himan") && pass.equals("7777"))?"로그인성공":"로그인실패";
+		System.out.println("결과 : " + result);
 		d.addAttribute("result", result);
 		return "WEB-INF\\views\\a06_login.jsp";
 	}
